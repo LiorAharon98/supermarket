82eb001875160bf5e3833a3e3c1b7f2854dea256
LiorAharon98 <lior.ah98@gmail.com>
2022-08-28 23:27:32 +0300

    update5

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const userRouter = require("./routes/user.route");
 const { setServerConfiguration } = require("./config");
 
 setServerConfiguration(app);
-mongoose.connect("mongodb://localhost:27017/shopping-cart-data-base");
+mongoose.connect(process.env.port);
 
 app.use("/shopping-cart", productRouter);
 app.use("/user", userRouter);
