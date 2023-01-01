b8cb693b5e7e5c24decba20d3fcdfd3c997a63e3
LiorAharon98 <lior.ah98@gmail.com>
2023-01-01 17:01:04 +0200

    update20

--- server/server.js
+++ server/server.js
@@ -4,15 +4,13 @@ const mongoose = require("mongoose");
 const productRouter = require("./routes/product.route");
 const userRouter = require("./routes/user.route");
 const { setServerConfiguration } = require("./config");
+require("dotenv").config();
 
 setServerConfiguration(app);
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://liors-database:lior.ah98@cluster0.iybrzvm.mongodb.net/supermarket?retryWrites=true&w=majority"
-);
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use("/supermarket", productRouter);
 
 app.use("/supermarket/user", userRouter);
 
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT);
